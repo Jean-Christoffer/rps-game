9877ab22528ab3a8110499c2f9f5124c502d2b79
half6 <christofferhalf6@gmail.com>
2023-03-15 09:15:14 +0100

    converted calculate winner to object

--- rock-paper-scissors/src/components/calculateWinner.jsx
+++ rock-paper-scissors/src/components/calculateWinner.jsx
@@ -1,51 +1,19 @@
-export default function calculateWinner(setWinner,setScore, setComputerScore,player,computer,settle){
-    if(player === computer){
-        setWinner((prevState)=>{
-          prevState = 'Draw'
-          return prevState
-        })
-  
-      }
-      else if(player === 'rock' && computer ==='scissors'){
-        setWinner((prevState)=>{
-          prevState = 'Player'
-          return prevState
-        })
-        setScore(prevCount => {
-         prevCount +=1
-         return prevCount
-        })
-  
-      }else if(player === 'paper' && computer ==='rock'){
-        setWinner((prevState)=>{
-          prevState = 'Player'
-          return prevState
-        })
-        setScore(prevCount => {
-           prevCount +=1
-           return prevCount
-          })
-  
-      }else if(player === 'scissors' && computer ==='paper'){
-        setWinner((prevState)=>{
-          prevState = 'Player'
-          return prevState
-        })
-        setScore(prevCount => {
-           prevCount +=1
-           return prevCount
-          })
-  
-      }else{
-        setWinner((prevState)=>{
-          prevState = 'Computer'
-          return prevState
-        })
-        setComputerScore((prevState)=>{
-          prevState +=1
-          return prevState
-        })
-  
-      }
-      settle(true)
+export default function calculateWinner(setWinner, setScore, setComputerScore, player, computer, settle) {
+  const winners = {
+    rock: 'scissors',
+    paper: 'rock',
+    scissors: 'paper',
+  };
+
+  if (player === computer) {
+    setWinner('Draw');
+  } else if (winners[player] === computer) {
+    setWinner('Player');
+    setScore(prevCount => prevCount + 1);
+  } else {
+    setWinner('Computer');
+    setComputerScore(prevCount => prevCount + 1);
+  }
+
+  settle(true);
 }
